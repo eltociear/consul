536aa64bd78ff12d97454b23379c27379bb9ab56
R.B. Boyer <rb@hashicorp.com>
2023-10-05 15:57:24 -0500

    removed suite

--- test-integ/catalogv2/simple_test.go
+++ test-integ/catalogv2/simple_test.go
@@ -13,11 +13,9 @@ import (
 	"github.com/hashicorp/consul/proto-public/pbresource"
 	libassert "github.com/hashicorp/consul/test/integration/consul-container/libs/assert"
 	"github.com/hashicorp/consul/test/integration/consul-container/libs/utils"
-	"github.com/hashicorp/consul/testing/deployer/sprawl"
 	"github.com/hashicorp/consul/testing/deployer/sprawl/sprawltest"
 	"github.com/hashicorp/consul/testing/deployer/topology"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 
 	"github.com/hashicorp/consul/test-integ/topoutil"
 )
@@ -44,27 +42,199 @@ func getImages() topology.Images {
 	return images
 }
 
-func TestSimpleTopologySuite(t *testing.T) {
-	suite.Run(t, new(SimpleTopologySuite))
-}
+func TestBasic_L4_ExplicitDestination(t *testing.T) {
+	topologyConfigAddNodes := func(
+		cluster *topology.Cluster,
+		nodeName func() string,
+		partition,
+		namespace string,
+	) {
+		clusterName := cluster.Name
 
-type SimpleTopologySuite struct {
-	suite.Suite
-	sprawl *sprawl.Sprawl
-}
+		newServiceID := func(name string) topology.ServiceID {
+			return topology.ServiceID{
+				Partition: partition,
+				Namespace: namespace,
+				Name:      name,
+			}
+		}
 
-var _ suite.SetupAllSuite = (*SimpleTopologySuite)(nil)
+		tenancy := &pbresource.Tenancy{
+			Partition: partition,
+			Namespace: namespace,
+			PeerName:  "local",
+		}
 
-func (s *SimpleTopologySuite) SetupSuite() {
-	cfg := s.topologyConfig(s.T())
-	s.sprawl = sprawltest.Launch(s.T(), cfg)
-}
+		singleportServerNode := &topology.Node{
+			Kind:      topology.NodeKindDataplane,
+			Version:   topology.NodeVersionV2,
+			Partition: partition,
+			Name:      nodeName(),
+			Services: []*topology.Service{
+				topoutil.NewFortioServiceWithDefaults(
+					clusterName,
+					newServiceID("single-server"),
+					topology.NodeVersionV2,
+					nil,
+				),
+			},
+		}
+		singleportClientNode := &topology.Node{
+			Kind:      topology.NodeKindDataplane,
+			Version:   topology.NodeVersionV2,
+			Partition: partition,
+			Name:      nodeName(),
+			Services: []*topology.Service{
+				topoutil.NewFortioServiceWithDefaults(
+					clusterName,
+					newServiceID("single-client"),
+					topology.NodeVersionV2,
+					func(svc *topology.Service) {
+						delete(svc.Ports, "grpc")     // v2 mode turns this on, so turn it off
+						delete(svc.Ports, "http-alt") // v2 mode turns this on, so turn it off
+						svc.Upstreams = []*topology.Upstream{{
+							ID:           newServiceID("single-server"),
+							PortName:     "http",
+							LocalAddress: "0.0.0.0", // needed for an assertion
+							LocalPort:    5000,
+						}}
+					},
+				),
+			},
+		}
+		singleportTrafficPerms := sprawltest.MustSetResourceData(t, &pbresource.Resource{
+			Id: &pbresource.ID{
+				Type:    pbauth.TrafficPermissionsType,
+				Name:    "single-server-perms",
+				Tenancy: tenancy,
+			},
+		}, &pbauth.TrafficPermissions{
+			Destination: &pbauth.Destination{
+				IdentityName: "single-server",
+			},
+			Action: pbauth.Action_ACTION_ALLOW,
+			Permissions: []*pbauth.Permission{{
+				Sources: []*pbauth.Source{{
+					IdentityName: "single-client",
+					Namespace:    namespace,
+				}},
+			}},
+		})
 
-func (s *SimpleTopologySuite) TestBasicExplicitDestination() {
-	t := s.T()
+		multiportServerNode := &topology.Node{
+			Kind:      topology.NodeKindDataplane,
+			Version:   topology.NodeVersionV2,
+			Partition: partition,
+			Name:      nodeName(),
+			Services: []*topology.Service{
+				topoutil.NewFortioServiceWithDefaults(
+					clusterName,
+					newServiceID("multi-server"),
+					topology.NodeVersionV2,
+					nil,
+				),
+			},
+		}
+		multiportClientNode := &topology.Node{
+			Kind:      topology.NodeKindDataplane,
+			Version:   topology.NodeVersionV2,
+			Partition: partition,
+			Name:      nodeName(),
+			Services: []*topology.Service{
+				topoutil.NewFortioServiceWithDefaults(
+					clusterName,
+					newServiceID("multi-client"),
+					topology.NodeVersionV2,
+					func(svc *topology.Service) {
+						svc.Upstreams = []*topology.Upstream{
+							{
+								ID:           newServiceID("multi-server"),
+								PortName:     "http",
+								LocalAddress: "0.0.0.0", // needed for an assertion
+								LocalPort:    5000,
+							},
+							{
+								ID:           newServiceID("multi-server"),
+								PortName:     "http-alt",
+								LocalAddress: "0.0.0.0", // needed for an assertion
+								LocalPort:    5001,
+							},
+						}
+					},
+				),
+			},
+		}
+		multiportTrafficPerms := sprawltest.MustSetResourceData(t, &pbresource.Resource{
+			Id: &pbresource.ID{
+				Type:    pbauth.TrafficPermissionsType,
+				Name:    "multi-server-perms",
+				Tenancy: tenancy,
+			},
+		}, &pbauth.TrafficPermissions{
+			Destination: &pbauth.Destination{
+				IdentityName: "multi-server",
+			},
+			Action: pbauth.Action_ACTION_ALLOW,
+			Permissions: []*pbauth.Permission{{
+				Sources: []*pbauth.Source{{
+					IdentityName: "multi-client",
+					Namespace:    namespace,
+				}},
+			}},
+		})
+
+		cluster.Nodes = append(cluster.Nodes,
+			singleportClientNode,
+			singleportServerNode,
+			multiportClientNode,
+			multiportServerNode,
+		)
+
+		cluster.InitialResources = append(cluster.InitialResources,
+			singleportTrafficPerms,
+			multiportTrafficPerms,
+		)
+	}
+
+	cfg := func() *topology.Config {
+		const clusterName = "dc1"
+
+		servers := topoutil.NewTopologyServerSet(clusterName+"-server", 3, []string{clusterName, "wan"}, nil)
+
+		cluster := &topology.Cluster{
+			Enterprise: utils.IsEnterprise(),
+			Name:       clusterName,
+			Nodes:      servers,
+		}
+
+		lastNode := 0
+		nodeName := func() string {
+			lastNode++
+			return fmt.Sprintf("%s-box%d", clusterName, lastNode)
+		}
+
+		topologyConfigAddNodes(cluster, nodeName, "default", "default")
+		if cluster.Enterprise {
+			topologyConfigAddNodes(cluster, nodeName, "part1", "default")
+			topologyConfigAddNodes(cluster, nodeName, "part1", "nsa")
+			topologyConfigAddNodes(cluster, nodeName, "default", "nsa")
+		}
+
+		return &topology.Config{
+			Images: getImages(),
+			Networks: []*topology.Network{
+				{Name: clusterName},
+				{Name: "wan", Type: "wan"},
+			},
+			Clusters: []*topology.Cluster{
+				cluster,
+			},
+		}
+	}()
+
+	sp := sprawltest.Launch(t, cfg)
 
 	var (
-		sp       = s.sprawl
 		asserter = topoutil.NewAsserter(sp)
 
 		topo    = sp.Topology()
@@ -120,194 +290,4 @@ func (s *SimpleTopologySuite) TestBasicExplicitDestination() {
 			check(t, ship)
 		})
 	}
-}
-
-func (s *SimpleTopologySuite) topologyConfig(t *testing.T) *topology.Config {
-	const clusterName = "dc1"
-
-	servers := topoutil.NewTopologyServerSet(clusterName+"-server", 3, []string{clusterName, "wan"}, nil)
-
-	cluster := &topology.Cluster{
-		Enterprise: utils.IsEnterprise(),
-		Name:       clusterName,
-		Nodes:      servers,
-	}
-
-	lastNode := 0
-	nodeName := func() string {
-		lastNode++
-		return fmt.Sprintf("%s-box%d", clusterName, lastNode)
-	}
-
-	s.topologyConfigAddNodes(t, cluster, nodeName, "default", "default")
-	if cluster.Enterprise {
-		s.topologyConfigAddNodes(t, cluster, nodeName, "part1", "default")
-		s.topologyConfigAddNodes(t, cluster, nodeName, "part1", "nsa")
-		s.topologyConfigAddNodes(t, cluster, nodeName, "default", "nsa")
-	}
-
-	return &topology.Config{
-		Images: getImages(),
-		Networks: []*topology.Network{
-			{Name: clusterName},
-			{Name: "wan", Type: "wan"},
-		},
-		Clusters: []*topology.Cluster{
-			cluster,
-		},
-	}
-}
-
-func (s *SimpleTopologySuite) topologyConfigAddNodes(
-	t *testing.T,
-	cluster *topology.Cluster,
-	nodeName func() string,
-	partition,
-	namespace string,
-) {
-	clusterName := cluster.Name
-
-	newServiceID := func(name string) topology.ServiceID {
-		return topology.ServiceID{
-			Partition: partition,
-			Namespace: namespace,
-			Name:      name,
-		}
-	}
-
-	tenancy := &pbresource.Tenancy{
-		Partition: partition,
-		Namespace: namespace,
-		PeerName:  "local",
-	}
-
-	singleportServerNode := &topology.Node{
-		Kind:      topology.NodeKindDataplane,
-		Version:   topology.NodeVersionV2,
-		Partition: partition,
-		Name:      nodeName(),
-		Services: []*topology.Service{
-			topoutil.NewFortioServiceWithDefaults(
-				clusterName,
-				newServiceID("single-server"),
-				topology.NodeVersionV2,
-				nil,
-			),
-		},
-	}
-	singleportClientNode := &topology.Node{
-		Kind:      topology.NodeKindDataplane,
-		Version:   topology.NodeVersionV2,
-		Partition: partition,
-		Name:      nodeName(),
-		Services: []*topology.Service{
-			topoutil.NewFortioServiceWithDefaults(
-				clusterName,
-				newServiceID("single-client"),
-				topology.NodeVersionV2,
-				func(svc *topology.Service) {
-					delete(svc.Ports, "grpc")     // v2 mode turns this on, so turn it off
-					delete(svc.Ports, "http-alt") // v2 mode turns this on, so turn it off
-					svc.Upstreams = []*topology.Upstream{{
-						ID:           newServiceID("single-server"),
-						PortName:     "http",
-						LocalAddress: "0.0.0.0", // needed for an assertion
-						LocalPort:    5000,
-					}}
-				},
-			),
-		},
-	}
-	singleportTrafficPerms := sprawltest.MustSetResourceData(t, &pbresource.Resource{
-		Id: &pbresource.ID{
-			Type:    pbauth.TrafficPermissionsType,
-			Name:    "single-server-perms",
-			Tenancy: tenancy,
-		},
-	}, &pbauth.TrafficPermissions{
-		Destination: &pbauth.Destination{
-			IdentityName: "single-server",
-		},
-		Action: pbauth.Action_ACTION_ALLOW,
-		Permissions: []*pbauth.Permission{{
-			Sources: []*pbauth.Source{{
-				IdentityName: "single-client",
-				Namespace:    namespace,
-			}},
-		}},
-	})
-
-	multiportServerNode := &topology.Node{
-		Kind:      topology.NodeKindDataplane,
-		Version:   topology.NodeVersionV2,
-		Partition: partition,
-		Name:      nodeName(),
-		Services: []*topology.Service{
-			topoutil.NewFortioServiceWithDefaults(
-				clusterName,
-				newServiceID("multi-server"),
-				topology.NodeVersionV2,
-				nil,
-			),
-		},
-	}
-	multiportClientNode := &topology.Node{
-		Kind:      topology.NodeKindDataplane,
-		Version:   topology.NodeVersionV2,
-		Partition: partition,
-		Name:      nodeName(),
-		Services: []*topology.Service{
-			topoutil.NewFortioServiceWithDefaults(
-				clusterName,
-				newServiceID("multi-client"),
-				topology.NodeVersionV2,
-				func(svc *topology.Service) {
-					svc.Upstreams = []*topology.Upstream{
-						{
-							ID:           newServiceID("multi-server"),
-							PortName:     "http",
-							LocalAddress: "0.0.0.0", // needed for an assertion
-							LocalPort:    5000,
-						},
-						{
-							ID:           newServiceID("multi-server"),
-							PortName:     "http-alt",
-							LocalAddress: "0.0.0.0", // needed for an assertion
-							LocalPort:    5001,
-						},
-					}
-				},
-			),
-		},
-	}
-	multiportTrafficPerms := sprawltest.MustSetResourceData(t, &pbresource.Resource{
-		Id: &pbresource.ID{
-			Type:    pbauth.TrafficPermissionsType,
-			Name:    "multi-server-perms",
-			Tenancy: tenancy,
-		},
-	}, &pbauth.TrafficPermissions{
-		Destination: &pbauth.Destination{
-			IdentityName: "multi-server",
-		},
-		Action: pbauth.Action_ACTION_ALLOW,
-		Permissions: []*pbauth.Permission{{
-			Sources: []*pbauth.Source{{
-				IdentityName: "multi-client",
-				Namespace:    namespace,
-			}},
-		}},
-	})
-
-	cluster.Nodes = append(cluster.Nodes,
-		singleportClientNode,
-		singleportServerNode,
-		multiportClientNode,
-		multiportServerNode,
-	)
-
-	cluster.InitialResources = append(cluster.InitialResources,
-		singleportTrafficPerms,
-		multiportTrafficPerms,
-	)
 }
